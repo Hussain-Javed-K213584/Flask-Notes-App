38a1cad3fb54ef4b573ca53c3fea3ba656db9a1a
Muhammad Hussain Javed <k213584@nu.edu.pk>
2023-09-18 10:55:41 +0500

    Added required attribute in html

--- website/auth.py
+++ website/auth.py
@@ -4,14 +4,14 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', method=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
-@auth.route('/logout')
+@auth.route('/logout', method=['GET', 'POST'])
 def logout():
     return render_template('login.html')
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', method=['GET', 'POST'])
 def sign_up():
     return render_template('sign_up.html')
